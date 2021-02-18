e00be50dde6bf589f429e5dd766cd9488f8c0d18
kasukosu <44358577+kasukosu@users.noreply.github.com>
2021-02-18 12:51:34 +0200

    Saving progress

--- hate-app/src/components/create-post.jsx
+++ hate-app/src/components/create-post.jsx
@@ -1,11 +1,42 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const CreatePost = () => {
     
-    
+    const [blogpost, setBlogPost] = useState({
+        message:"", author:""
+    })
+
+    const addPost = () => {
+        const blogpost = {author:blogpost.author, message: blogpost.message}
+
+        if(blogpost.message && blogpost.message > 0){
+            axios.post('./api/posts', blogpost)
+                .then(res=> {
+                    if(res.data){
+                        setBlogPost({message: ""})
+                    }
+                })
+                .catch(err => console.log(err))
+        }else{
+            console.log('input field required')
+        }
+    }
+
+    const handleChange = (e) => {
+        setBlogPost({
+          message: e.target.value
+        })
+    }
+
     
     return ( 
         <section className="create-post">
             <h1>create-post</h1>
+            <form onSubmit={this.onFormSubmit}>
+                <input type="text"/>
+                <button type="submit">Submit</button>
+            </form>
         </section>
     );
 }
--- hate-app/src/components/postlist.jsx
+++ hate-app/src/components/postlist.jsx
@@ -1,10 +1,44 @@
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 const Postlist = () => {
     
+    const [postList, setPostList] = useState([]);
+
+    const getBlogPosts = () => {
+        axios.get('/api/posts')
+            .then(res => {
+                if(res.data){
+                    console.log(res.data);
+                    setPostList(res.data);
+                }
+            })
+    }
     
+    const deletePost = (id) => {
+        axios.delete(`/api/posts/${id}`)
+            .then(res => {
+                if(res.data){
+                    getBlogPosts();
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+    useEffect(()=> {
+        getBlogPosts();
+    },[])
+
+
     return ( 
         <section className="postfeed">
-            <h1>Post-list</h1>
+            {
+                postList.map(item => 
+                    <div>
+                        {item.message} 
+                    
+                    </div>
+                )
+            }
         </section>
     );
 }
