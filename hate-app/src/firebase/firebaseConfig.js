b077a7b17a0385d131403560a858222f81a67005
kasukosu <44358577+kasukosu@users.noreply.github.com>
2021-03-19 21:09:06 +0200

    fixed

--- hate-app/src/firebase/firebaseConfig.js
+++ hate-app/src/firebase/firebaseConfig.js
@@ -7,7 +7,7 @@ import 'firebase/storage';
 // Initialize Firebase
 let config = {
   apiKey: "AIzaSyA9mG7xdL1hRlxqaktyjEnDF2Xmz7sUPOk",
-  authDomain: "hatesome.com",
+  authDomain: "hatesome-app.firebaseapp.com",
   projectId: "hatesome-app",
   storageBucket: "hatesome-app.appspot.com",
   messagingSenderId: "312379445507",
